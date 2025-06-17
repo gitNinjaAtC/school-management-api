88c08f95f2a78319dd12ea4ffa338310660b91d7
gitNinjaAtC <dansachin436732danger@gmail.com>
2025-06-17 22:20:52 +0530

    final push

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
+const fs = require("fs");
 module.exports = {
   dbConfig: {
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "school_management",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+      ca: fs.readFileSync("./certs/ca.pem"),
+    },
   },
 };
